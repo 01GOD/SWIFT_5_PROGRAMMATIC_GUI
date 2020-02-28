e3b23a13c79a4194a00cb2c812845c4b18891f20
01GOD <OMNILOVESTAR@GMAIL.COM>
2020-02-28 19:14:19 +0800

    Initial Commit

---
+++ PROGRAMMATIC_GUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PROGRAMMATIC_GUI
+//
+//  Created by GOD on 2/28/20.
+//  Copyright © 2020 ALL ONE SUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
